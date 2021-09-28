de143a7b93009d13be0389a4b33ef13ff4f21f40
Sajan4072 <rai4072@gmail.com>
2021-09-29 00:07:10 +0545

    first api end point adduser

--- myproj/aapli/urls.py
+++ myproj/aapli/urls.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
 
-# urlpatterns = [
+from django.urls import path
+from . import views
+
+urlpatterns = [
     
-#     path('',),
-# ]
+    path("addUser",views.addUser,name="addUser"),
+]
--- myproj/aapli/views.py
+++ myproj/aapli/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def addUser(request):
+    val={
+        'response':'User Added'
+
+    }
+    print("hello addUser")
+    status=200
+    return JsonResponse(val,status=status)
